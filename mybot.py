5551f54b95f49215518f072f7da825f1c1d0d532
hydrogen2 <weizhiwei@gmail.com>
2018-11-11 11:47:43 +0800

    set up libs

--- mybot.py
+++ mybot.py
@@ -1,9 +1,12 @@
+import os
 from nltk.tag import StanfordPOSTagger
-from nltk.parse import malt
+from nltk.parse.malt import MaltParser
 from nltk.stem import WordNetLemmatizer
 
-tagger = StanfordPOSTagger('english-bidirectional-distsim.tagger', '/Users/weizhiwei/nlp/stanford-postagger-2018-10-16/stanford-postagger.jar')
-parser = malt.MaltParser('/Users/weizhiwei/nlp/maltparser-1.9.1', '/Users/weizhiwei/nlp/engmalt.linear-1.7.mco')
+os.environ['STANFORD_MODELS'] = 'libs/stanford-postagger-2018-10-16/models'
+
+tagger = StanfordPOSTagger('english-bidirectional-distsim.tagger', 'libs/stanford-postagger-2018-10-16/stanford-postagger.jar')
+parser = MaltParser(os.path.dirname(os.path.abspath(__file__))+'/libs/maltparser-1.9.2', 'libs/engmalt.linear-1.7.mco')
 stemmer = WordNetLemmatizer()
 
 kb = {}
@@ -16,9 +19,9 @@ while True:
     tagged = tagger.tag(line.split())
     parse = parser.parse_tagged_sents([tagged]).next().next()
     
-    parse.tree()
+    print parse.tree()
     
-    // walk the tree
-    // look up the words
-    // ask questions
-    // 
+    # walk the tree
+    # look up the words
+    # ask questions
+    #
