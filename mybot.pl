990e7b5952885aa5d4036fcb578ebe0027b5ad8b
wei zhiwei <zwei@eyeota.com>
2019-12-26 17:10:38 +0800

    add multiplication rule. not working yet

--- mybot.pl
+++ mybot.pl
@@ -4,15 +4,28 @@
 solve(FrameName, Entity) :-
     quantifiable(FrameName), solveQuant(FrameName, Entity).
 
-quantifiable(f:quant).
-addable(f:quant).
+quantifiable(f_number).
+addable(f_number).
+
+quantifiable(f_cost).
+addable(f_cost).
 
 solveQuant(FrameName, Entity) :- % known quant
     call(FrameName, F),
-    f:entity(F, Entity),
-    f:value(F, Val),
+    f_entity(F, Entity),
+    f_value(F, Val),
     makeVar(FrameName, Entity, Var),
     makeEquation(eq, Var, Val).
+
+solveQuant(FrameName, Total) :- % multiplication
+    addable(FrameName),
+    makeVar(f_number, Total, Var1),
+    mean(FrameName, Total, Mean),
+    makeVar(FrameName, Mean, Var2),
+    makeVar(FrameName, Total, Prod),
+    makeEquation(product, Var1, Var2, Prod),
+    solveQuant(f_number, Total),
+    solveQuant(FrameName, Mean).
 
 solveQuant(FrameName, Part) :- % addition
     addable(FrameName),
@@ -26,47 +39,69 @@ solveQuant(FrameName, Part) :- % addition
     maplist(solveQuant(FrameName), PartsExcludingThis),
     solveQuant(FrameName, Whole).
 
+mean(FrameName, Total, Mean) :-
+    f_each(F),
+    f_set(F, Total),
+    f_one(F, Mean).
+
 partition(Whole, Parts) :-
     partWhole(E1, Whole),
     partWhole(E2, Whole),
-    f:entity(P1, E1),
-    f:entity(P2, E2),
+    f_entity(P1, E1),
+    f_entity(P2, E2),
     negatePred(P1, P2),
     Parts = [E1, E2].
 
-makeVar(FrameName, Entity, Var) :- write('let '), write(Entity), write('.'), write(FrameName), writeln(' = x.'), Var = Entity.
+makeVar(FrameName, Entity, Var) :- atomic_list_concat([Entity, FrameName], '.', Var).
 makeEquation(eq, Var, Val) :- write(Var), write(' = '), writeln(Val).
-makeEquation(sum, Vars, Var) :- atomic_list_concat(Vars, ' + ', L), write(L), write(' = '), writeln(Var).
+makeEquation(sum, Vars, Sum) :- atomic_list_concat(Vars, ' + ', L), write(L), write(' = '), writeln(Sum).
+makeEquation(product, Var1, Var2, Prod) :- write(Var1), write('*'), write(Var2), write(' = '), writeln(Prod).
 
 partWhole(Part, Whole) :-
-    f:part_whole(F),
-    f:part(F, Part),
-    f:whole(F, Whole).
+    f_part_whole(F),
+    f_part(F, Part),
+    f_whole(F, Whole).
 
 pred(Node, Pred) :-
-    f:color(Pred),
-    f:entity(Pred, Node).
+    f_color(Pred),
+    f_entity(Pred, Node).
 
 predsEqual(P1, P2) :-
-    f:color(P1),
-    f:color(P2),
-    f:value(P1, C1),
-    f:value(P2, C2),
+    f_color(P1),
+    f_color(P2),
+    f_value(P1, C1),
+    f_value(P2, C2),
     C1 = C2.
 
 negatePred(P, NP) :- pnp(P, NP).
 negatePred(NP, P) :- pnp(P, NP).
 
-np(Pred) :- f:neg(F), f:pred(F, Pred).
+np(Pred) :- f_neg(F), f_pred(F, Pred).
 pnp(Pred, NegPred) :-
     predsEqual(Pred, NegPred),
     \+ np(Pred),
     np(NegPred).
 
-f:quant(f6). f:entity(f6, e1). f:value(f6, 8).
-f:quant(f7). f:entity(f7, e2). f:value(f7, 3).
-f:part_whole(f1). f:whole(f1, e1). f:part(f1, e2).
-f:color(f3). f:value(f3, brown). f:entity(f3, e2).
-f:part_whole(f2). f:whole(f2, e1). f:part(f2, e3).
-f:color(f4). f:value(f4, brown). f:entity(f4, e3).
-f:neg(f5). f:pred(f5, f4).
+f_number(f6). f_entity(f6, e1). f_value(f6, 8).
+f_number(f7). f_entity(f7, e2). f_value(f7, 3).
+f_part_whole(f1). f_whole(f1, e1). f_part(f1, e2).
+f_color(f3). f_value(f3, brown). f_entity(f3, e2).
+f_part_whole(f2). f_whole(f2, e1). f_part(f2, e3).
+f_color(f4). f_value(f4, brown). f_entity(f4, e3).
+f_neg(f5). f_pred(f5, f4).
+
+f_number(f8). f_entity(f8, e4). f_value(f8, 20).
+f_cost(f9). f_entity(f9, e4). f_value(f9, 180).
+f_part_whole(f10). f_whole(f10, e4). f_part(f10, e5).
+f_apple(f11). f_entity(f11, e5).
+f_part_whole(f12). f_whole(f12, e4). f_part(f12, e6).
+f_banana(f13). f_entity(f13, e6).
+
+f_each(f15). f_one(f15, e7). f_set(f15, e5).
+f_number(f18). f_entity(f18, e7). f_value(f18, 1).
+f_cost(f14). f_entity(f14, e7). f_value(f14, 10).
+
+f_each(f17). f_one(f17, e8). f_set(f17, e6).
+f_number(f19). f_entity(f19, e8). f_value(f19, 1).
+f_cost(f16). f_entity(f16, e8). f_value(f16, 8).
+partition(e4, [e5, e6]).
