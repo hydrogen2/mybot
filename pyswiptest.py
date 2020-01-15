98ce23e360edfd46e9da538715ef085d07ef8753
wei zhiwei <zwei@eyeota.com>
2020-01-15 17:56:31 +0800

    add implicit multiplication parsing

--- pyswiptest.py
+++ pyswiptest.py
@@ -7,13 +7,14 @@ import pyswip
 from pyswip import Prolog, registerForeign, Atom
 from sympy import *
 from sympy.solvers.solveset import nonlinsolve
-from sympy.parsing.sympy_parser import parse_expr
+from sympy.parsing.sympy_parser import (parse_expr, standard_transformations, implicit_multiplication)
 
 pyswip.easy.PL_unify_atom_chars = pyswip.core._lib.PL_unify_atom_chars
+transformations = standard_transformations + (implicit_multiplication,)
 
 def w_nonlinsolve(*a):
     if True: #isinstance(a[0], Atom) and isinstance(a[1], Atom):
-        eqs = [parse_expr(s) for s in str(a[0]).split()]
+        eqs = [parse_expr(s, transformations=transformations) for s in str(a[0]).split()]
         xs = symbols(str(a[1]))
         ans = nonlinsolve(eqs, xs)
         a[2].value = str(ans.args[0])
@@ -25,4 +26,4 @@ registerForeign(w_nonlinsolve, arity=3)
 
 prolog = Prolog()
 # prolog.consult('mybot.pl')
-print(list(prolog.query("Eqs='x+y-180 a+b-20 5*a-x 15*b-y', Xs='a b x y', w_nonlinsolve(Eqs, Xs, Ans)", catcherrors=False)))
+print(list(prolog.query("Eqs='3(a+2b)-(27) 5a+10b-x', Xs='a b x', w_nonlinsolve(Eqs, Xs, Ans)", catcherrors=False)))
